2bc9fea4fabafa987e3231356a00d494328ac8d4
Aditya Maulana <dait23@gmail.com>
2018-10-08 23:45:16 +0700

    update api

--- src/views/Api/Api.js
+++ src/views/Api/Api.js
@@ -1,6 +1,6 @@
 
 ///// Config
 
-export const MainApi = 'http://10.0.1.12:88/fkppi/public/api/';
+export const MainApi = 'http://13.76.193.84/public/api/';
 
-export const DevApi = 'http://10.0.1.12:88/fkppi/public/api/';
+export const DevApi = 'http://13.76.193.84/public/api/';
